b91961e829bacf7dfff374a87000aebdc4e6150e
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-10-13 02:31:44 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -16,5 +16,7 @@ def speak_to_grandma(phrase)
     puts "I LOVE YOU TOO PUMPKIN!"
   elsif "#{phrase}".upcase == "phrase"
     puts "HUH?! SPEAK UP, SONNY!"
+  else
+    puts "HUH?! SPEAK UP, SONNY!"
   end
 end
